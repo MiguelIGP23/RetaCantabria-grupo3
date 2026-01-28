e294537ad84b8e9c5e5a18a78b05c335d90975d2
paco11111 <152623847+paco11111@users.noreply.github.com>
2026-01-28 16:50:22 +0100

    +Usuario: Controller + Service

---
+++ JavaApp/src/main/java/org/example/javaapp/controller/UsuarioController.java
@@ -0,0 +1,43 @@
+package org.example.javaapp.controller;
+
+import org.example.javaapp.model.Usuario;
+import org.example.javaapp.service.ServiceUsuario;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/usuario")
+public class UsuarioController {
+
+    private ServiceUsuario service;
+
+    public UsuarioController(ServiceUsuario service){
+        this.service=service;
+    }
+
+    @PostMapping
+    public Usuario insert(@RequestBody Usuario usuario){
+        return service.insert(usuario);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable int id){
+        service.delete(id);
+    }
+
+    @PutMapping("/{id}")
+    public Usuario update(@PathVariable int id, @RequestBody Usuario usuario){
+        return service.update(id, usuario);
+    }
+
+    @GetMapping("/{id}")
+    public Usuario findById(@PathVariable int id){
+        return service.findById(id);
+    }
+
+    @GetMapping
+    public List<Usuario> findAll(){
+        return service.findAll();
+    }
+}
---
+++ JavaApp/src/main/java/org/example/javaapp/service/IServiceUsuario.java
@@ -0,0 +1,6 @@
+package org.example.javaapp.service;
+
+import org.example.javaapp.model.Usuario;
+
+public interface IServiceUsuario extends IService<Usuario, Integer>{
+}
