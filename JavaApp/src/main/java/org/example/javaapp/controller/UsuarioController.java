10b8e57d6c84dae6dd28b66226a30c2f074c1926
Daniel <dcabezab18@gmail.com>
2026-01-29 15:21:30 +0100

    Usuario: Añadida etiqueta @Service a su service y cambiada la direccion del endpoint para tener consistencia con el resto

--- JavaApp/src/main/java/org/example/javaapp/controller/UsuarioController.java
+++ JavaApp/src/main/java/org/example/javaapp/controller/UsuarioController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/usuario")
+@RequestMapping("/api/reta3/usuario")
 public class UsuarioController {
 
     private ServiceUsuario service;
