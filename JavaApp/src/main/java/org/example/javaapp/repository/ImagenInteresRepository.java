18491c7ebd5f1edee7deaae7ba755a47d469cf07
Daniel <dcabezab18@gmail.com>
2026-01-29 17:59:24 +0100

    ImagenInteres: Controller terminado

---
+++ JavaApp/src/main/java/org/example/javaapp/repository/ImagenInteresRepository.java
@@ -0,0 +1,7 @@
+package org.example.javaapp.repository;
+
+import org.example.javaapp.model.Imagenesintere;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImagenInteresRepository extends JpaRepository<Imagenesintere,Integer> {
+}
