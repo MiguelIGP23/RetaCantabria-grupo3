0617025309e0c8de8e54b9acfbe7772c0a298ecc
MiguelIGP23 <minglesg2401@educantabria.es>
2026-01-30 19:00:52 +0100

    Paquete model con entidades completo

---
+++ CSharpApp/Models/ImagenPeligro.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class ImagenPeligro
+    {
+        [JsonPropertyName("idimagenes_peligro")]
+        public int Idimagenes_peligro { get; set; }
+
+        [JsonPropertyName("url")]
+        public string Url { get; set; }
+
+        [JsonPropertyName("descripcion")]
+        public string Descripcion { get; set; }
+
+        [JsonPropertyName("puntospeligro_idPuntosinteres")]
+        public int Puntospeligro_idPuntosinteres { get; set; }
+
+        // Constructor por defecto (one-line)
+        public ImagenPeligro() { }
+
+        // Constructor completo (firma one-line)
+        public ImagenPeligro(int idimagenes_peligro, string url, string descripcion, int puntospeligro_idPuntosinteres)
+        {
+            Idimagenes_peligro = idimagenes_peligro;
+            Url = url;
+            Descripcion = descripcion;
+            Puntospeligro_idPuntosinteres = puntospeligro_idPuntosinteres;
+        }
+    }
+}
---
+++ CSharpApp/Models/Trackpoint.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Trackpoint
+    {
+        [JsonPropertyName("id")]
+        public TrackpointId Id { get; set; }
+
+        [JsonPropertyName("latitud")]
+        public double Latitud { get; set; }
+
+        [JsonPropertyName("longitud")]
+        public double Longitud { get; set; }
+
+        public Trackpoint() { }
+
+        public Trackpoint(TrackpointId id, double latitud, double longitud)
+        {
+            Id = id;
+            Latitud = latitud;
+            Longitud = longitud;
+        }
+    }
+}
