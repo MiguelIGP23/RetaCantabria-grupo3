7d2080d40e7b5b93a2e8affd3d9d23e1c25c1a49
Daniel <dcabezab18@gmail.com>
2026-02-02 15:19:20 +0100

    Fix builder de ruta devolviendo uno de ImagenesInteres

--- KotlinApp/app/src/main/java/com/example/kotlinapp/data/ServiceFactory.kt
+++ KotlinApp/app/src/main/java/com/example/kotlinapp/data/ServiceFactory.kt
@@ -1,10 +1,12 @@
 package com.example.kotlinapp.data
 
+import com.example.kotlinapp.model.Ruta
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ServiceFactory {
-    private const val URL = "https://api.raspiremote.org/api/reta3"
+    //private const val URL = "https://api.raspiremote.org/api/reta3"
+    private const val URL = "http://10.0.2.2:8080/api/reta3/"
     private val DEFAULT_BUILDER = Retrofit.Builder()
         .baseUrl(URL)
         //usar convertidor de JSON
@@ -22,8 +24,8 @@ object ServiceFactory {
     fun puntosPeligro(): PuntoPeligroService {
         return DEFAULT_BUILDER.build().create(PuntoPeligroService::class.java)
     }
-    fun ruta(): ImagenInteresService {
-        return DEFAULT_BUILDER.build().create(ImagenInteresService::class.java)
+    fun ruta(): RutaService {
+        return DEFAULT_BUILDER.build().create(RutaService::class.java)
     }
     fun trackpoint(): TrackpointService {
         return DEFAULT_BUILDER.build().create(TrackpointService::class.java)
