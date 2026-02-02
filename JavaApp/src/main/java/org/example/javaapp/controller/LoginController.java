a3d7bfb8eb8109af000d4dbf016f00883e536223
MiguelIGP23 <minglesg2401@educantabria.es>
2026-02-02 22:42:24 +0100

    -Login implementado
    -Todos los endpoints bloqueados menos /auth/login.
    -Implementada lógica de búsqueda y comparación de email y contraseña en la bd.
    -Implementado cifrado seguro mediante BCrypt.
    -Creado Repository, Service y Controller para el login.

--- JavaApp/src/main/java/org/example/javaapp/controller/LoginController.java
+++ JavaApp/src/main/java/org/example/javaapp/controller/LoginController.java
@@ -1,9 +1,35 @@
 package org.example.javaapp.controller;
 
+import org.example.javaapp.dto.LoginRequest;
+import org.example.javaapp.model.Usuario;
+import org.example.javaapp.repository.UsuarioRepository;
+import org.example.javaapp.service.ServiceLogin;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/reta3/auth/login")
+@RequestMapping("/api/reta3/auth/")
 public class LoginController {
+
+    private final ServiceLogin service;
+
+    public LoginController(ServiceLogin service) {
+        this.service=service;
+    }
+
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequest request){
+        Usuario usuario = service.validarLogin(request.getEmail(), request.getPassword());
+
+        if(usuario==null){
+            return ResponseEntity.status(401).body("Credenciales incorrectas");
+        }
+
+        return ResponseEntity.ok(("LOGIN OK -> rol: "+usuario.getRol()));
+    }
 }
--- JavaApp/src/main/java/org/example/javaapp/service/ServiceLogin.java
+++ JavaApp/src/main/java/org/example/javaapp/service/ServiceLogin.java
@@ -1,4 +1,28 @@
 package org.example.javaapp.service;
 
+import org.example.javaapp.model.Usuario;
+import org.example.javaapp.repository.UsuarioRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ServiceLogin {
+
+    private final UsuarioRepository usuarioRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public ServiceLogin(UsuarioRepository usuarioRepository, PasswordEncoder passwordEncoder) {
+        this.usuarioRepository = usuarioRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public Usuario validarLogin(String email, String password) {
+        Usuario usuario = usuarioRepository.findByEmail(email).orElse(null);
+        if (usuario == null) return null;
+
+        boolean ok = passwordEncoder.matches(password, usuario.getPassword());
+        if (!passwordEncoder.matches(password, usuario.getPassword())) return null;
+
+        return usuario; // login correcto
+    }
 }
